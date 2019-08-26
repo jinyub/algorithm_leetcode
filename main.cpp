7613bc5a6c3b0553043ff916d479969fdb3642f5
xpju <xpju1104@163.com>
2019-08-26 09:41:44 +0800

    滑动窗口

--- main.cpp
+++ main.cpp
@@ -10,7 +10,45 @@ using namespace std;
 #include <algorithm>
 #include <string.h>
 
-
+//找滑动窗口中的最大值
+//这种思路相当于求固定size中的最大值，时间复杂度为O(n*size)
+vector<int> maxInWindows(const vector<int>& num, unsigned int size)
+{
+    vector<int> res;
+    if(num.empty()||size==0)
+        return res;
+    int max=INT_MIN,start=1,max_index=0;
+    for (int j = 0; j < size; ++j) {
+        if(num[j]>max){
+            max = num[j];
+            max_index = j;
+        }
+    }
+    res.push_back(max);
+    for (int i = start; i < num.size()-size+1; ++i) {
+        if(max_index!=i-1){
+            if(num[i+size-1]>max){
+                max = num[i+size-1];
+                max_index = i+size-1;
+            }
+            res.push_back(max);
+        }else{
+            max = INT_MIN;
+            for (int j = i; j < i+size-1; ++j) {
+                if(num[j]>max){
+                    max = num[j];
+                    max_index = j;
+                }
+            }
+            if(num[i+size-1]>max){
+                max = num[i+size-1];
+                max_index = i+size-1;
+            }
+            res.push_back(max);
+        }
+    }
+    return res;
+}
 
 int main() {
     return 0;
